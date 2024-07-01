61b78862fe40bee2118d034a0a7790632cf59188
Anand Kumar <anandkumarmca6@gmail.com>
2024-07-02 00:24:03 +0530

    Create product api

--- app/app.js
+++ app/app.js
@@ -4,14 +4,16 @@ import express from 'express';
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/usersRoute.js';
 import { globalErrHandler, notFound } from '../middlewares/globalErrHandler.js';
+import productsRouter from '../routes/productRoute.js';
 dbConnect();
+
 const app = express();
 
 app.use(express.json());
 
 // routes
 app.use('/api/v1/users/',userRoutes);
-
+app.use('/api/v1/products/',productsRouter);
 //err middleware handler
 app.use(notFound);
 app.use(globalErrHandler);
---
+++ controllers/productsCtrl.js
@@ -0,0 +1,28 @@
+import Product from "../model/Product.js";
+import asyncHandler from "express-async-handler";
+export const createProduct = asyncHandler(async(req,res)=>{
+    const {name, description, category,sizes,colors,user,price,totalQty,brand} = req.body;
+    // product already exists
+    const productExists = await Product.findOne({name});
+    if(productExists){
+        throw new Error("Product already exists");
+    }
+    // create new product
+    const product = await Product.create({
+        name,
+        description,
+        category,
+        sizes,
+        colors,
+        user:req.userAuthId,
+        price,
+        totalQty,
+        brand
+    });
+    // push new product into category
+    res.status(201).json({
+        status:"success",
+        message:"Product created successfully",
+        product,
+    })
+})
